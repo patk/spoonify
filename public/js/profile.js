c71711ebf79c8a0e9a02975fbb0b39d9c2f126f3
Pat <pat.ksarach@gmail.com>
2021-04-07 16:11:16 +1000

    Add food diary section to the profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,6 +6,8 @@ $("#profile-link").toggleClass("active");
 var calIntakeOption = {
   chart: {
     type: "bar",
+    height: 280,
+    fontFamily: "Poppins, sans-serif",
   },
   series: [
     {
@@ -13,6 +15,16 @@ var calIntakeOption = {
       data: [1850, 2100, 1990, 2000, 1780, 2290, 2170],
     },
   ],
+  fill: {
+    colors: ["#c32222"],
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      type: "vertical",
+      gradientToColors: ["#fcb97d"],
+      stops: [0, 100],
+    },
+  },
   xaxis: {
     categories: [
       "2021-04-05",
@@ -36,10 +48,21 @@ calIntake.render();
 // nutrition intake chart
 var nutritionIntakeOption = {
   chart: {
-    type: "pie",
+    type: "donut",
+    height: 280,
+    fontFamily: "Poppins, sans-serif",
   },
   series: [6, 1, 2],
   labels: ["Easy", "Medium", "Difficult"],
+  colors: ["#66ccff", "#6699ff", "#6666ff", "#6633ff"],
+  stroke: {
+    show: false,
+  },
+  dataLabels: {
+    dropShadow: {
+      enabled: false,
+    },
+  },
 };
 
 var nutritionIntake = new ApexCharts(
